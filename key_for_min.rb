a6930416af74028cba07d994df48d5f06a5a522e
Clayon Stewart <c.stewart89@gmail.com>
2021-03-09 03:55:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
     name_hash.each do |key2, value2|
       puts "Comparing #{key}: #{value} with #{key2}: #{value2}"
       if value <= value2
-        puts 
         item = key
+        puts key
       end
     end
   end
