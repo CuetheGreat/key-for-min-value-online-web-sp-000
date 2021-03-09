f879fcf39b93bc2c6f3cf09845e0fab153642a0e
Clayon Stewart <c.stewart89@gmail.com>
2021-03-09 03:50:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  item = 
+  item = ""
   name_hash.each do |key, value|
     name_hash.each do |key2, value2|
       if value <= value2
