2ad08179effc43ff8d42b52560fca1762c77e936
Clayon Stewart <c.stewart89@gmail.com>
2021-03-09 03:56:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  item = ""
-  name_hash.each do |key, value|
-    name_hash.each do |key2, value2|
-      puts "Comparing #{key}: #{value} with #{key2}: #{value2}"
-      if value <= value2
-        item = key
-        puts key
-      end
-    end
-  end
-  item
+  
 end
